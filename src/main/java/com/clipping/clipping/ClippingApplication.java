352c3ed0e17870000f4403cf375eab6ca3c6752a
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-06-08 01:29:26 -0300

    initial commit

---
+++ src/main/java/com/clipping/clipping/ClippingApplication.java
@@ -0,0 +1,13 @@
+package com.clipping.clipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClippingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clipping/clipping/ClippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clipping.clipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
