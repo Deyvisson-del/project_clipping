0c1e82040eab014ecf166dc70d03986efd0e0c62
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-06-15 01:51:12 -0300

    Criação da classe Repository

--- src/main/java/com/clipping/clipping/repository/ClippingRepository.java
+++ src/main/java/com/clipping/clipping/repository/ClippingRepository.java
@@ -1,4 +1,14 @@
 package com.clipping.clipping.repository;
 
-public interface ClippingRepository {
+import com.clipping.clipping.model.ClippingModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime; // Importação adicionada
+import java.util.List;
+
+@Repository
+public interface ClippingRepository extends JpaRepository<ClippingModel, Long> {
+    List<ClippingModel> findByPalavraBusca(String palavraBusca);
+    List<ClippingModel> findByDataBuscaAfter(LocalDateTime data);
 }
