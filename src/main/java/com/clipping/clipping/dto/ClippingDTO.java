7cb6d92fdca240fd61d6f11a0934318cda93fe5c
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-06-15 02:17:54 -0300

    criação do DTO

--- src/main/java/com/clipping/clipping/dto/ClippingDTO.java
+++ src/main/java/com/clipping/clipping/dto/ClippingDTO.java
@@ -1,4 +1,13 @@
 package com.clipping.clipping.dto;
 
+import java.time.LocalDateTime;
+
 public class ClippingDTO {
+    private String tituloNoticia;
+    private String autorNoticia;
+    private String url;
+    private String fonteNoticia;
+    private LocalDateTime dataPublicacao;
+    private LocalDateTime dataBusca = LocalDateTime.now();
+    private String palavraBusca;
 }
