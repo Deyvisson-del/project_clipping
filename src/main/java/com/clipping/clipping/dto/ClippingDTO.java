e52592dcba4396c5216c14f6512a15dc665e4ba6
Deyvisson_Rafael <129315687+Deyvisson-del@users.noreply.github.com>
2025-06-15 02:21:25 -0300

    Revert "criação do DTO"

--- src/main/java/com/clipping/clipping/dto/ClippingDTO.java
+++ src/main/java/com/clipping/clipping/dto/ClippingDTO.java
@@ -1,13 +1,4 @@
 package com.clipping.clipping.dto;
 
-import java.time.LocalDateTime;
-
 public class ClippingDTO {
-    private String tituloNoticia;
-    private String autorNoticia;
-    private String url;
-    private String fonteNoticia;
-    private LocalDateTime dataPublicacao;
-    private LocalDateTime dataBusca = LocalDateTime.now();
-    private String palavraBusca;
 }
